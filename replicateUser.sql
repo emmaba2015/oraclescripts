8574a82a3d3712d887386eace232edcbbc4ce6dc
Ezequiel E. Tejada <ezequieltejada@users.noreply.github.com>
2015-07-29 11:27:56 -0300

    Agregado SQL output
    Agregado SQL output para que ya cree el script para regenerar el usuario.

--- replicateUser.sql
+++ replicateUser.sql
@@ -3,6 +3,7 @@ DEFINE USER = &1
 set head off
 set pages 0
 set long 9999999
+spool userDDL.sql
 SELECT DBMS_METADATA.GET_DDL('USER', &USER) || '/' DDL
 FROM dual
 UNION ALL
@@ -14,3 +15,4 @@ FROM Dual
 UNION ALL
 SELECT DBMS_METADATA.GET_GRANTED_DDL('OBJECT_GRANT', &USER) || '/' DDL
 FROM Dual;
+spool off
